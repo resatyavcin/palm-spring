7ff288245b9e44287157f14214cffd8d50f92e44
Reşat Yavçin <resatyavcin@icloud.com>
2026-02-05 21:29:04 +0300

    chore: initialize project

---
+++ src/main/java/com/resatyavcin/palm/PalmApplication.java
@@ -0,0 +1,13 @@
+package com.resatyavcin.palm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/resatyavcin/palm/PalmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resatyavcin.palm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
